4a5f4120d0bf44c85cc8ccc6d01a8f7e7c60bb14
justanuragmaurya <justanuragmaurya@gmail.com>
2024-10-08 08:51:37 +0530

    added signup page (signup backend not done yet)

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -71,7 +71,7 @@ export default function Signup() {
             </Button>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <Link href="#" className="underline">
+              <Link href="/signup" className="underline">
                 Sign up
               </Link>
             </div>
